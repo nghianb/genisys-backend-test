482affd45c6bb9f891521c7c1a516ee349f05a9e
nghianb <nghianb.it@gmail.com>
2021-11-20 21:09:51 +0700

    Forwards request params to destination url

--- app/Http/Controllers/URLShortenerController.php
+++ app/Http/Controllers/URLShortenerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\UrlShortener\ShortenUrlResolver;
+use Illuminate\Http\Request;
 
 class URLShortenerController extends Controller
 {
@@ -14,10 +15,28 @@ class URLShortenerController extends Controller
         $this->shortenUrlResolver = $shortenUrlResolver;
     }
     
-    public function __invoke($alias)
+    public function __invoke($alias, Request $request)
     {
         $shortenUrl = $this->shortenUrlResolver->resolve($alias);
 
-        return redirect($shortenUrl->destination_url);
+        return $this->redirectWithAdditionalRequestParams(
+            $shortenUrl->destination_url,
+            $request->query()
+        );
+    }
+
+    protected function redirectWithAdditionalRequestParams(
+        $destinationUrl,
+        array $additionalRequestParams
+    ) {
+        $query = parse_url($destinationUrl, PHP_URL_QUERY);
+
+        if ($query) {
+            $destinationUrl .= '&'.http_build_query($additionalRequestParams);
+        } else {
+            $destinationUrl .= '?'.http_build_query($additionalRequestParams);
+        }
+
+        return redirect($destinationUrl);
     }
 }
