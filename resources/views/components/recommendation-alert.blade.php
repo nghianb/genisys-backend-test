3502b58d3083801bb01610128ea3e4c4df86eb94
nghianb <nghianb.it@gmail.com>
2021-11-21 11:17:34 +0700

    Show recommendation alert with only guest

--- resources/views/components/recommendation-alert.blade.php
+++ resources/views/components/recommendation-alert.blade.php
@@ -1,10 +1,12 @@
-<div class="row alert alert-primary" role="alert">
-    <div class="col-lg-9">
-        <h3>Want more? Try Premium Features!</h3>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem aut corrupti, hic eum iure quasi consequatur perferendis cum quaerat fugit.</p>
+@guest
+    <div class="row alert alert-primary" role="alert">
+        <div class="col-lg-9">
+            <h3>Want more? Try Premium Features!</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem aut corrupti, hic eum iure quasi consequatur perferendis cum quaerat fugit.</p>
+        </div>
+        <div class="col-lg-3 text-center">
+            <a href="{{ route('login.show') }}" class="btn btn-outline-success btn-lg">Go Premium</a>
+            <p  class="mt-3">or <a href="{{ route('register.show') }}">Sign up free!</a></p>
+        </div>
     </div>
-    <div class="col-lg-3 text-center">
-        <a class="btn btn-outline-success btn-lg" href="#" role="button">Go Premium</a>
-        <p  class="mt-3">or <a href="">Sign up free!</a></p>
-    </div>
-</div>
+@endguest
